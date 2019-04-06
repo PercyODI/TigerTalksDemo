9b35c105f5e85fcad607821618849e380a052ab5
PercyODI <ccl7k3@mizzou.edu>
2019-04-06 13:58:38 -0500

    Added no option

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return 'Hello World in a whole new way!'
+	
+@app.route('/no')
+def no():
+	return 'You said no'
